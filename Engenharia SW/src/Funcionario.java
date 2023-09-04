c013e4e25c310145841d5a1bdc00303d48762f6d
Luis Eduardo Plácido Guimarães <127263371+LuisPGuimaraes@users.noreply.github.com>
2023-09-04 20:07:08 -0300

    Inclusao Aividade diagrama casos

---
+++ Engenharia SW/src/Funcionario.java
@@ -0,0 +1,25 @@
+
+public class Funcionario {
+
+	private string id;
+
+	public string getId() {
+		return id;
+	}
+
+	public void setId(string id) {
+		this.id = id;
+	}
+
+	public Funcionario(string id) {
+		
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
