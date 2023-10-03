cf7097862f8ea5a24e0d814350ce66070ec0c2c7
Luis Eduardo Plácido Guimarães <127263371+LuisPGuimaraes@users.noreply.github.com>
2023-10-02 21:14:21 -0300

    Criação do projeto
    Adição das primeiras classes

---
+++ Engenharia SW/ProjetoClube/src/Entities.java
@@ -0,0 +1,32 @@
+public class Entities {
+
+    public class Funcionario() {
+        double incricao;
+
+        public Str getNome() {
+            return Socio.nome;
+        }
+
+        public void setNome(Str incricao) {
+            this.socio.nome = socio.nome;
+        }
+
+        public double getValidade() {
+            return Socio.validade;
+        }
+
+        public void setValidade() {
+            this.socio.validade = socio.validade;
+        }
+
+        public Str getPlano() {
+            return socio.plano;
+        }
+
+        public void setPlano() {
+            this.socio.plano = socio.plano;
+        }
+
+
+    }
+}
