e5554afd114189d95d998dc696a6e0907fb46100
Luis Eduardo Plácido Guimarães <luisegsilva.le@gmail.com>
2023-10-09 20:05:57 -0300

    Manutenção métodos contrutores

--- Engenharia SW/ProjetoClube/src/Entities.java
+++ Engenharia SW/ProjetoClube/src/Entities.java
@@ -3,12 +3,16 @@ public class Entities {
     public class Funcionario() {
         double incricao;
 
-        public Str getNome() {
+        public Funcionario (inscricao) {
+            this.inscricao = inscricao;
+        }
+
+        public String getNome() {
             return Socio.nome;
         }
 
-        public void setNome(Str incricao) {
-            this.socio.nome = socio.nome;
+        public void setNome(String incricao) {
+            this.Socio.nome = Socio.nome;
         }
 
         public double getValidade() {
@@ -16,17 +20,37 @@ public class Entities {
         }
 
         public void setValidade() {
-            this.socio.validade = socio.validade;
+            this.Socio.validade = Socio.validade;
         }
 
-        public Str getPlano() {
-            return socio.plano;
+        public String getPlano() {
+            return Socio.plano;
         }
 
         public void setPlano() {
-            this.socio.plano = socio.plano;
+            this.Socio.plano = Socio.plano;
+        }
+    }
+        public boolean(){
+        if (inscricao)
         }
 
+    public static class Socio(){
+        public String nome;
+        public date validade;
+        public String plano;
+        public String dataHoje;
+    }
 
+    public class Clube(){
+        private list <Socio> ();
+
+        private boolean socio_entrada(Socio){
+            if (Socio.validade > date){
+                return True;}
+            else {
+                return False;}
+
+        }
     }
 }
--- Engenharia SW/ProjetoClube/src/Main.java
+++ Engenharia SW/ProjetoClube/src/Main.java
@@ -1,3 +1,5 @@
+java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
