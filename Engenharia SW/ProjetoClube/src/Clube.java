274d857dcdbb404b266894b6f6f9a4d44645240d
Luis Eduardo Plácido Guimarães <luisegsilva.le@gmail.com>
2023-11-06 20:26:32 -0300

    update main

--- Engenharia SW/ProjetoClube/src/Clube.java
+++ Engenharia SW/ProjetoClube/src/Clube.java
@@ -1,4 +1,27 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Clube {
+
+    private List<String> listaSocios; // Lista de sócios do clube
+
+    // Construtor para inicializar a lista de sócios
+    public Clube() {
+        this.listaSocios = new ArrayList<>(); // Inicializa a lista como um ArrayList vazio
+    }
+
+    // Método para obter a lista de sócios
+    public List<String> getListaSocios() {
+        return listaSocios;
+    }
+
+    // Método para definir a lista de sócios
+    public void setListaSocios(List<String> listaSocios) {
+        this.listaSocios = listaSocios;
+    }
+
+    // Método para adicionar um novo sócio à lista
+    public void adicionarSocio(String novoSocio) {
+        listaSocios.add(novoSocio); // Adiciona o novo sócio à lista de sócios
+    }
 }
--- Engenharia SW/ProjetoClube/src/Main.java
+++ Engenharia SW/ProjetoClube/src/Main.java
@@ -1,14 +1,19 @@
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static <Int> void main(String[] args){
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-aaaa");
+        String entrada_socio;
+        String entrada_plano;
+        int entrada_inscricao;
+        String dados_socio;
+
+        Clube clube = new Clube();
 
         Socio socio1 = new Socio("Fernando",new SimpleDateFormat("12-12-2023"),"Basico", 0001);
-
+        Socio socio2 = new Socio("Beatriz", new SimpleDateFormat("11-05-2023"), "Premium", 0002);
 
         System.out.print("Digite seu nome: ");
         String nome_func = sc.nextLine();
@@ -25,8 +30,29 @@ public class Main {
         if (registrador == 0){
             System.out.println("Até mais!!!");
         }
-        elif (registrador == 1){
-            System.out.println();
+        else if (registrador == 1);{
+            System.out.println("Digite o nome do sócio:");
+            entrada_socio = sc.next();
+            System.out.println("Digite o plano:");
+            entrada_plano = sc.next();
+            System.out.println("Digite o número de inscrição:");
+            entrada_inscricao = sc.nextInt();
+
+            if (socio3 == false){
+                
+            }
+
+            Socio
+            //dados_socio = "1" + entrada_socio + "2" + entrada_plano + "3" + entrada_inscricao;
+            //clube.adicionarSocio(dados_socio);
+
+            //System.out.println(clube.getListaSocios());
+           // System.out.println("_____Plano Gold_____");
+
+
+
+
+
         }
 
 
