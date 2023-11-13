bcabbd70f9bdeba9ead315b07d28c79f1fdc4394
Luis Eduardo Plácido Guimarães <luisegsilva.le@gmail.com>
2023-11-13 11:46:26 -0300

    :sparkles: feat: Liberar acessos por plano

--- Engenharia SW/ProjetoClube/src/Main.java
+++ Engenharia SW/ProjetoClube/src/Main.java
@@ -46,32 +46,69 @@ public class Main {
                 System.out.println("Nome: " + numerosocio.getNome());
                 System.out.println("Plano: " + numerosocio.getPlano());
                 System.out.println("Validade: " + numerosocio.getValidade());
-            } else if (registrador == 2) {
+            }
+            else if (registrador == 2) {
                 System.out.println("_____Consultar Sócio_____");
                 System.out.println();
                 System.out.println("Digite o número de inscrição: ");
                 String selecionar_inscricao = sc.next();
-                for (Socio numerosocio : clube.getListaSocios()){
+                for (Socio numerosocio : clube.getListaSocios()) {
                     if (Objects.equals(numerosocio.getInscricao(), selecionar_inscricao)) {
-                        System.out.println(numerosocio);
-                        break;}
-                }
+                        System.out.println(numerosocio.getNome());
 
+                        if (Objects.equals(numerosocio.getPlano(), "GOLD")) {
+                            System.out.println("_____PLANO GOLD_____");
+                            System.out.println("Acessos:");
+                            System.out.println("1. Quadra poliesportiva");
+                            System.out.println("2. Piscina");
+                            System.out.println("3. Salão de jogos");
+                            System.out.println("4. Academia");
+                            System.out.println("0. Voltar");
+                            System.out.println();
+                            System.out.println("Insira qual acesso deseja: ");
 
-                //System.out.println("Número de inscrição: " + numerosocio.getInscricao());
-                //System.out.println("Nome: " + numerosocio.getNome());
-                //System.out.println("Plano: " + numerosocio.getPlano());
-                //System.out.println("Validade: " + numerosocio.getValidade());
-            }
+                            while (true) {
+                                int registrador2 = sc.nextInt();
+                                if (registrador2==1){
+                                    System.out.println("_____Acesso liberado_____");
+                                    System.out.println();
+                                    System.out.println("_____QUADRA_____");
+                                }
+                                else if (registrador2==2) {
+                                    System.out.println("_____Acesso liberado_____");
+                                    System.out.println();
+                                    System.out.println("_____PISCINA_____");
+                                }
+                                else if (registrador2==3) {
+                                    System.out.println("_____Acesso liberado_____");
+                                    System.out.println();
+                                    System.out.println("_____SALÃO DE JOGOS_____");
+                                }
+                                else if (registrador2==4) {
+                                    System.out.println("_____Acesso liberado_____");
+                                    System.out.println();
+                                    System.out.println("_____ACADEMIA_____");
+                                }
+                                if (registrador2==0) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }}
             else if (registrador == 3) {
                 System.out.println("teste");
                 System.out.println(clube.getListaSocios());
             }
-                else if (registrador == 0) {
+            else if (registrador == 0) {
                 System.out.println("Até mais!!!");
-                break;
-            }
+                break;}
+             }
 
 
 
-        }}}
+
+
+
+
+    }}
