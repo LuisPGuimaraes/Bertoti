cb3de6e5cb3da66cc37654d49161520abd12658c
Luis Eduardo Plácido Guimarães <luisegsilva.le@gmail.com>
2023-11-13 00:15:05 -0300

    :bug: Fix: Loop do menu de escolha

--- Engenharia SW/ProjetoClube/src/Main.java
+++ Engenharia SW/ProjetoClube/src/Main.java
@@ -2,7 +2,7 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Main {
-    public static <Int> void main(String[] args){
+    public static <Int> void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-aaaa");
         String entrada_socio;
@@ -12,7 +12,7 @@ public class Main {
 
         Clube clube = new Clube();
 
-        Socio socio1 = new Socio("Fernando",new SimpleDateFormat("12-12-2023"),"Basico", 0001);
+        Socio socio1 = new Socio("Fernando", new SimpleDateFormat("12-12-2023"), "Basico", 0001);
         Socio socio2 = new Socio("Beatriz", new SimpleDateFormat("11-05-2023"), "Premium", 0002);
 
         System.out.print("Digite seu nome: ");
@@ -20,40 +20,35 @@ public class Main {
         Funcionario funcionario = new Funcionario(nome_func);
         funcionario.setNome_func(nome_func);
         System.out.println();
-        System.out.println("BEM VINDO!" + funcionario.getNome_func() );
+        System.out.println("BEM VINDO!" + funcionario.getNome_func());
         System.out.println();
         System.out.println();
-        System.out.println("O que deseja fazer?");
-        System.out.println("1. Cadastro Novo Sócio");
-        System.out.println("0. Sair");
-        int registrador = sc.nextInt();
-        if (registrador == 0){
-            System.out.println("Até mais!!!");
-        }
-        else if (registrador == 1);{
-            System.out.println("Digite o nome do sócio:");
-            entrada_socio = sc.next();
-            System.out.println("Digite o plano:");
-            entrada_plano = sc.next().toUpperCase();
-            System.out.println("Digite o número de inscrição:");
-            entrada_inscricao = sc.nextInt();
-            System.out.println();
+        
+        while (true) {
+            System.out.println("O que deseja fazer?");
+            System.out.println("1. Cadastro Novo Sócio");
+            System.out.println("0. Sair");
+            int registrador = sc.nextInt();
+            if (registrador == 1) {
+                System.out.println("Digite o nome do sócio:");
+                entrada_socio = sc.next();
+                System.out.println("Digite o plano:");
+                entrada_plano = sc.next().toUpperCase();
+                System.out.println("Digite o número de inscrição:");
+                entrada_inscricao = sc.nextInt();
 
-            Socio numerosocio = new Socio(entrada_socio, new SimpleDateFormat("01-01-2024"), entrada_plano,entrada_inscricao);
-            System.out.println("Novo usuário cadastrado:");
-            System.out.println("Número de inscrição: " + numerosocio.getInscricao());
-            System.out.println("Nome: " + numerosocio.getNome());
-            System.out.println("Plano: " + numerosocio.getPlano());
-            System.out.println("Validade: " + numerosocio.getValidade());
+                Socio numerosocio = new Socio(entrada_socio, new SimpleDateFormat("01-01-2024"), entrada_plano, entrada_inscricao);
+                System.out.println("Novo usuário cadastrado:");
+                System.out.println("Número de inscrição: " + numerosocio.getInscricao());
+                System.out.println("Nome: " + numerosocio.getNome());
+                System.out.println("Plano: " + numerosocio.getPlano());
+                System.out.println("Validade: " + numerosocio.getValidade());
             }
-            
-
-
-
-
-
+            else if (registrador ==0){
+                break;
+            }
         }
-
-
+        System.out.println("Até mais!!!");
 
     }
+}
