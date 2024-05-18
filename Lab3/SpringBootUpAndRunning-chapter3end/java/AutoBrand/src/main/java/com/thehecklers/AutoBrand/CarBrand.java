9567a8c34c350655eea048f63eea5827e8d11eb1
Luis Eduardo Plácido Guimarães <luisegsilva.le@gmail.com>
2024-05-18 01:50:24 -0300

    new features

---
+++ Lab3/SpringBootUpAndRunning-chapter3end/java/AutoBrand/src/main/java/com/thehecklers/AutoBrand/CarBrand.java
@@ -0,0 +1,52 @@
+package com.thehecklers.AutoBrand;
+
+import java.util.List;
+
+public class CarBrand {
+    private String name;
+    private String country;
+    private String abbreviation;
+
+    public CarBrand(String name, String country, String abbreviation){
+        this.name = name;
+        this.country = country;
+        this.abbreviation = abbreviation;
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+    public Object getId() {
+        return null;
+    }
+
+    public void add(CarBrand carBrands) {
+    }
+
+    public int indexOf(CarBrand c) {
+        return 0;
+    };
+
+    public void setCars(int brandIndex, CarBrand carBrands) {
+    }
+}
