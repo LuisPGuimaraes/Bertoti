2dce16bc3a49230a3327a8d6e252398a3183a24a
Luis Eduardo Plácido Guimarães <luisegsilva.le@gmail.com>
2024-05-28 10:59:11 -0300

    Structure for brand System

---
+++ Lab III/AutoBrandSystem/java/AutoBrand/src/main/java/com/thehecklers/AutoBrand/AutoBrand.java
@@ -0,0 +1,55 @@
+package com.thehecklers.AutoBrand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SpringBootApplication
+public class AutoBrand {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AutoBrand.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/brands")
+class RestApiDemoController {
+	private List<CarBrand> carBrands = new ArrayList<>();
+
+	public RestApiDemoController() {
+		carBrands.addAll(List.of(
+				new CarBrand("General Motors","Estados Unidos","GM"),
+				new CarBrand("Volkswagen","Alemanha","VW"),
+				new CarBrand("Aston Martin","Reino Unido","AM"),
+				new CarBrand("Rolls Royce","Reino Unido","RR"),
+				new CarBrand("Maserati","Italia","MT")));
+	}
+
+	@GetMapping
+	Iterable<CarBrand> getBrand() {
+		return carBrands;
+	}
+
+	@GetMapping("/{abbreviation}")
+	List<CarBrand> getAbbreviation(){
+        return carBrands;
+    };
+
+	@PostMapping
+	CarBrand postBrand(@RequestBody CarBrand carBrands) {
+		carBrands.add(carBrands);
+		return carBrands;
+	}
+
+	@DeleteMapping("/{abbreviation}")
+	void deleteBrand(@PathVariable String abbreviation) {
+		carBrands.removeIf(c -> c.getAbbreviation().equals(abbreviation));
+	}
+}
---
+++ Lab III/AutoBrandSystem/java/AutoBrand/src/test/java/com/thehecklers/AutoBrand/AutoBrandTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.AutoBrand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBrandTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
